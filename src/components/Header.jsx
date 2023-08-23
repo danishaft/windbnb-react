45ef45586d7d589a9b0dadcc63fc691d854cc5bc
Danishaft <danielejeh2019@gmail.com>
2023-08-23 08:52:55 +0100

    worked on the header to manage state in the component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -91,54 +91,59 @@ const Header = () => {
                 </div>
             </div>
         </header>
-        <div className={`modal ${formData.setModal}`}>
-            <div className="modal__top">
-                <span>Edit your search</span>
-                <span id="close" className="setModal" onClick={modal}><RxCross2 /></span>
-            </div>
-            <form action="">
-                <div className="modal__form">
-                    <div onClick={() => activate("item1")} className="input__data">
-                        <input id="location" type="text" name="location" value={formData.location} readOnly disabled />
-                        <label htmlFor="">LOCATION</label>
-                    </div>
-                    <div onClick={() => activate("item2")} className="input__data">
-                        <input id="guests" type="text" name="guests" value={sum > 0 ? ` ${sum} guests` : "Add guests"} readOnly disabled/>
-                        <label htmlFor="">GUESTS</label>
-                    </div>
+        <div className="modal__container">
+            <div className={`modal ${formData.setModal}`}>
+                <div className="modal__top">
+                    <span>Edit your search</span>
+                    <span id="close" className="setModal" onClick={modal}><RxCross2 /></span>
                 </div>
+                <form action="">
+                    <div className="modal__form">
+                        <div onClick={() => activate("item1")} className="input__data">
+                            <input id="location" type="text" name="location" value={formData.location} readOnly />
+                            <label htmlFor="">LOCATION</label>
+                        </div>
+                        <div onClick={() => activate("item2")} className="input__data">
+                            <input id="guests" type="text" name="guests" value={sum > 0 ? ` ${sum} guests` : "Add guests"} readOnly/>
+                            <label htmlFor="">GUESTS</label>
+                        </div>
+                        <div className="input__data tablet">
+                            <button type="submit" onClick={handleSubmit}><BsSearch className="submit__icon" />Search</button>
+                        </div>
+                    </div>
 
-                
-                <div className="options">
-                    <div id="option--location"  className={`options__location ${formData.activeItem === 'item1' ? 'flex' : 'close'}`}>
-                        <span className="location" onClick={handleForm}><FaLocationDot className="location__icon"/>Helsinki, Finland</span>
-                        <span className="location" onClick={handleForm}><FaLocationDot className="location__icon"/>Turku, Finland</span>
-                        <span className="location" onClick={handleForm}><FaLocationDot className="location__icon"/>Oulu, Finland</span>
-                        <span className="location" onClick={handleForm}><FaLocationDot className="location__icon"/>Vaasa, Finland</span>
-                    </div>
-                    <div id="option--guests" className={`options__guests ${formData.activeItem === 'item2' ? 'open' : 'close'}`}>
-                        <div className="guests--type">
-                            <h3>Adults</h3>
-                            <p>Ages 13 or above</p>
-                            <div className="counter">
-                                <button className="minus" type="button" onClick={addGuests} data-test-id="adultCount">-</button>
-                                <span>{formData.adultCount}</span>
-                                <button className="add" type="button" onClick={addGuests} data-test-id="adultCount">+</button>
+                    
+                    <div className="options">
+                        <div id="option--location"  className={`options__location ${formData.activeItem === 'item1' ? 'flex' : 'close'}`}>
+                            <span className="location" onClick={handleForm}><FaLocationDot className="location__icon"/>Helsinki, Finland</span>
+                            <span className="location" onClick={handleForm}><FaLocationDot className="location__icon"/>Turku, Finland</span>
+                            <span className="location" onClick={handleForm}><FaLocationDot className="location__icon"/>Oulu, Finland</span>
+                            <span className="location" onClick={handleForm}><FaLocationDot className="location__icon"/>Vaasa, Finland</span>
+                        </div>
+                        <div id="option--guests" className={`options__guests ${formData.activeItem === 'item2' ? 'open' : 'close'}`}>
+                            <div className="guests--type">
+                                <h3>Adults</h3>
+                                <p>Ages 13 or above</p>
+                                <div className="counter">
+                                    <button className="minus" type="button" onClick={addGuests} data-test-id="adultCount">-</button>
+                                    <span>{formData.adultCount}</span>
+                                    <button className="add" type="button" onClick={addGuests} data-test-id="adultCount">+</button>
+                                </div>
+                            </div>
+                            <div className="guests--type">
+                                <h3>Children</h3>
+                                <p>Ages 2-12</p>
+                                <div className="counter">
+                                    <button className="minus" type="button" onClick={addGuests} data-test-id="childrenCount">-</button>
+                                    <span>{formData.childrenCount}</span>
+                                    <button className="add" type="button" onClick={addGuests} data-test-id="childrenCount">+</button>
+                                </div>
                             </div>
                         </div>
-                        <div className="guests--type">
-                            <h3>Children</h3>
-                            <p>Ages 2-12</p>
-                            <div className="counter">
-                                <button className="minus" type="button" onClick={addGuests} data-test-id="childrenCount">-</button>
-                                <span>{formData.childrenCount}</span>
-                                <button className="add" type="button" onClick={addGuests} data-test-id="childrenCount">+</button>
-                            </div>
-                        </div>
                     </div>
-                </div>
-                <button type="submit" onClick={handleSubmit}><BsSearch className="submit__icon" />Search</button>
-            </form>
+                    <button id="mobile" type="submit" onClick={handleSubmit}><BsSearch className="submit__icon" />Search</button>
+                </form>
+            </div>
         </div>
        </>
         
